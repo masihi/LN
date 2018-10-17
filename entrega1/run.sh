cb7656e2159ed424321a1bfe38352fa90f5ada24
masihi <madrugadjn@gmail.com>
2018-10-17 21:50:35 +0100

    Add slash trasnducer

--- entrega1/run.sh
+++ entrega1/run.sh
@@ -3,9 +3,10 @@
 rm -f *.fst *.pdf
 
 # Compila e gera a versão gráfica do transdutor que traduz SET -> 09
-#fstcompile --isymbols=letras.sym --osymbols=numeros.sym  mmm2mm.txt | fstarcsort > mmm2mm.fst
-#fstdraw    --isymbols=letras.sym --osymbols=numeros.sym --portrait mmm2mm.fst | dot -Tpdf  > mmm2mm.pdf
-
-# Compila e gera a versão gráfica do transdutor que traduz SET -> 09
 fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  mmm2mm.txt | fstarcsort > mmm2mm.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait mmm2mm.fst | dot -Tpdf  > mmm2mm.pdf
+
+
+# Compila e gera a versão gráfica do transdutor que processa a barra
+fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  slash.txt | fstarcsort > slash.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait slash.fst | dot -Tpdf  > slash.pdf
