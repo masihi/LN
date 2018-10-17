8ae13daf06bbffcff8b158cfd8ff2c39f511e348
masihi <madrugadjn@gmail.com>
2018-10-18 00:31:36 +0100

    Finally did it, 1b

--- entrega1/run.sh
+++ entrega1/run.sh
@@ -18,3 +18,21 @@ fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait days.fst | dot
 # Compila e gera a versão gráfica do transdutor que processa os anos de 2001...2099
 fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  years.txt | fstarcsort > years.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait years.fst | dot -Tpdf  > years.pdf
+
+# Concatenate transducers to create a transducer capable of reading 1/SET/2001 1/09/2001
+
+fstconcat days.fst slash.fst  > misto2numerico1.fst
+fstproject --project_output misto2numerico1.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico1.fst | dot -Tpdf  > misto2numerico1.pdf
+
+fstconcat misto2numerico1.fst mmm2mm.fst  > misto2numerico2.fst
+fstproject --project_output misto2numerico2.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico2.fst | dot -Tpdf  > misto2numerico2.pdf
+
+fstconcat misto2numerico2.fst slash.fst  > misto2numerico3.fst
+fstproject --project_output misto2numerico3.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico3.fst | dot -Tpdf  > misto2numerico3.pdf
+
+fstconcat misto2numerico3.fst years.fst > misto2numerico.fst
+fstproject --project_output misto2numerico.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico.fst | dot -Tpdf  > misto2numerico.pdf
