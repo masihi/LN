518471f3a3c367512e3e2d5fcc21331a44241e1e
masihi <madrugadjn@gmail.com>
2018-10-18 00:56:01 +0100

    finished b

--- entrega1/run.sh
+++ entrega1/run.sh
@@ -22,17 +22,37 @@ fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait years.fst | do
 # Concatenate transducers to create a transducer capable of reading 1/SET/2001 1/09/2001
 
 fstconcat days.fst slash.fst  > misto2numerico1.fst
-fstproject --project_output misto2numerico1.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+#fstproject --project_output misto2numerico1.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico1.fst | dot -Tpdf  > misto2numerico1.pdf
 
 fstconcat misto2numerico1.fst mmm2mm.fst  > misto2numerico2.fst
-fstproject --project_output misto2numerico2.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+#fstproject --project_output misto2numerico2.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico2.fst | dot -Tpdf  > misto2numerico2.pdf
 
 fstconcat misto2numerico2.fst slash.fst  > misto2numerico3.fst
-fstproject --project_output misto2numerico3.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+#fstproject --project_output misto2numerico3.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico3.fst | dot -Tpdf  > misto2numerico3.pdf
 
 fstconcat misto2numerico3.fst years.fst > misto2numerico.fst
-fstproject --project_output misto2numerico.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
+#fstproject --project_output misto2numerico.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait misto2numerico.fst | dot -Tpdf  > misto2numerico.pdf
+
+
+# Compila e gera a versão gráfica do transdutor que traduz os meses de ingles para portugues SEP -> SET
+fstcompile --isymbols=syms.txt --osymbols=syms-out.txt  enmmmpor.txt | fstarcsort > enmmmpor.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait enmmmpor.fst | dot -Tpdf  > enmmmpor.pdf
+
+#Concatena transdutores pare criar um transdutor capaz de traduzir converter uma data no formato condensado misto, de inglês
+#para português: Exemplo: "20/SEP/2018" é convertida em "20/SET/2018";
+
+fstconcat misto2numerico1.fst enmmmpor.fst  > en2pt1.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait en2pt1.fst | dot -Tpdf  > en2pt1.pdf
+
+fstconcat en2pt1.fst slash.fst  > en2pt2.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait en2pt2.fst | dot -Tpdf  > en2pt2.pdf
+
+fstconcat en2pt2.fst years.fst  > en2pt.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait en2pt.fst | dot -Tpdf  > en2pt.pdf
+
+fstinvert en2pt.fst pt2en.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms-out.txt --portrait pt2en.fst | dot -Tpdf  > pt2en.pdf
